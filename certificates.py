4ff828d4450d8279ec78fa928aef87afcd41e4fd
Satyarth Mishra Sharma <github@satyarth.me>
2017-12-20 21:05:13 +0000

    REmove exra line break

--- certificates.py
+++ certificates.py
@@ -43,7 +43,6 @@ class Validator:
         return verify(root, signature, self.public_key)
 
     def _check_revoked(self, certificate):
-
         response = requests.post(self.base_url + "check_root",
                                  data={"certificate_id": certificate.id}).json()
 
